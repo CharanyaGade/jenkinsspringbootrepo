405c4f761b326da49c0ec5462dd36e0843871d3d
Charanya Gade <charanyagade@gmail.com>
2025-08-09 10:42:56 +0530

    updated project

--- src/main/java/com/klef/ClientController.java
+++ src/main/java/com/klef/ClientController.java
@@ -14,5 +14,9 @@ public class ClientController {
 	public String demo() {
 		return "Jenkins Demo";
 	}
+	@GetMapping("/test")
+	public String test() {
+		return "Deploy SpringBoot using Jenkins";
+	}
 
 }
